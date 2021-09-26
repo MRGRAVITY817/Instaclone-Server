44c14b3e2b9ba3564b75e4ca1ffd394f83c6eab4
SeongHoon Wee <mrgravity817@gmail.com>
2021-09-26 17:35:34 +0900

    Update SeePhotoLikes API to return more simple result

--- src/photos/seePhotoLikes/seePhotoLikes.typeDefs.ts
+++ src/photos/seePhotoLikes/seePhotoLikes.typeDefs.ts
@@ -1,12 +1,7 @@
 import { gql } from 'apollo-server';
 
 export default gql`
-  type SeePhotoLikesResult {
-    ok: Boolean!
-    error: String
-    users: [User]
-  }
   type Query {
-    seePhotoLikes(id: Int!): SeePhotoLikesResult!
+    seePhotoLikes(id: Int!): [User]!
   }
 `;
